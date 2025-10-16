3c23a54f13a905d6f2d08cb4a9a0f8a8ebf8c9a0
Fabien ALLADE <alladefabien@gmail.com>
2025-10-16 14:26:45 +0200

    finish module creation

--- modules/tls_certificate/main.tf
+++ modules/tls_certificate/main.tf
@@ -12,7 +12,7 @@ resource "aws_acm_certificate" "main" {
 
 resource "aws_route53_record" "validation" {
   for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       type   = dvo.resource_record_type
       record = dvo.resource_record_value
@@ -27,6 +27,6 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_acm_certificate_validation" "main" {
-  certificate_arn         = aws_acm_certificate.this.arn
+  certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
